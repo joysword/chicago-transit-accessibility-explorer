2a60dd9e2ee6f7aa255c7b198e2cb5e2b4427390
Shi Yin <joysword@me.com>
2014-08-04 11:51:43 -0500

    added requesting geojson data

--- acc.py
+++ acc.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
+import json
+
+import os
+
 app = Flask(__name__)
 app.debug = True
 
@@ -9,6 +13,27 @@ def access():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/showmap')
+def show_map():
+
+	transit = request.args.get('type')
+	time = request.args.get('time')
+	threshold = request.args.get('threshold')
+	filt = request.args.get('filter')
+	age = request.args.get('age')
+	earning = request.args.get('earning')
+	industry = request.args.get('industry')
+	race = request.args.get('race')
+	ethnicity = request.args.get('ethnicity')
+	education = request.args.get('education')
+	gender = request.args.get('gender')
+
+	f = open(os.path.join(os.path.dirname(__file__), 'static/json/acc_auto_300.geojson'), 'r')
+
+	ret = json.load(f)
+
+	return jsonify(ret=ret)
 
 if __name__ == '__main__':
     app.run()
--- static/js/accessibility.js
+++ static/js/accessibility.js
@@ -6,18 +6,13 @@ $(window).resize(function () {
 }).resize();
 
 (function(){
-    var grid_layer = new L.FeatureGroup();
-    var jenks_cutoffs;
-    //var map = L.mapbox.map('map', 'joysword.i6b4jale').setView([41.8910,-87.6839], 11);
-    var map = L.map('map', {center:[41.8910,-87.6839], zoom: 11});
+    var map = L.map('map').fitBounds([[41.644286009999995, -87.94010087999999], [42.023134979999995, -87.52366115999999]]);;
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/joysword.i6b4jale/{z}/{x}/{y}.png', {
         attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
     }).addTo(map);
 
-
-    var drawnItems = new L.FeatureGroup();
-    map.addLayer(drawnItems);
+    var drawnItems = L.featureGroup().addTo(map);
 
     var drawControl = new L.Control.Draw({
         edit: {
@@ -57,22 +52,131 @@ $(window).resize(function () {
     map.on('draw:drawstart', draw_delete);
 
     function draw_create(e) {
-        this.drawnItems.clearLayers();
-        this.drawnItems.addLayer(e.layer);
+        drawnItems.clearLayers();
+        drawnItems.addLayer(e.layer);
         this.dataLayer = e.layer.toGeoJSON();
     }
 
     function draw_edit(e) {
-        this.drawnItems.clearLayers();
+        drawnItems.clearLayers();
     }
 
     function draw_edit(e) {
         var layers = e.layers;
-        this.drawnItems.clearLayers();
+        drawnItems.clearLayers();
         var self = this;
         layers.eachLayer(function(layer){
             self.dataLayer = layer.toGeoJSON();
             self.drawnItems.addLayer(layer);
         });
     }
+
+    function draw_delete(e){
+        drawnItems.clearLayers();
+    }
+
+    // which type? auto or transit
+    $('#select-type').change(function(){
+        switch (this.value) {
+            case "auto":
+                $('#form-time').addClass('no-disp');
+                break;
+            case "transit":
+                console.log('transit');
+                $('#form-time').removeClass('no-disp');
+                break;
+        }
+    });
+
+    // which filter? age, earning, industry, race, ethnicity, education or gender
+    $('#select-filter').change(function(){
+        switch (this.value) {
+            case "fi_age":
+                $('#form-age').attr('class', '');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_earning":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', '');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_industry":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', '');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_race":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', '');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_ethnicity":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', '');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_education":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', '');
+                $('#form-gender').attr('class', 'no-disp');
+                break;
+            case "fi_gender":
+                $('#form-age').attr('class', 'no-disp');
+                $('#form-earning').attr('class', 'no-disp');
+                $('#form-industry').attr('class', 'no-disp');
+                $('#form-race').attr('class', 'no-disp');
+                $('#form-ethnicity').attr('class', 'no-disp');
+                $('#form-education').attr('class', 'no-disp');
+                $('#form-gender').attr('class', '');
+                break;
+        }
+    });
+
+    function show_map(e) {
+        $('#map').spin({lines: 12, length: 0, width: 8, radius: 12});
+        $.getJSON($SCRIPT_ROOT + '/showmap', {
+            type: $('#select-type').val(),
+            time: $('#select-time').val(),
+            threshold: $('#select-threshold').val(),
+            filter: $('#select-filter').val(),
+            age: $('#select-age').val(),
+            earning: $('#select-earning').val(),
+            industry: $('#select-industry').val(),
+            race: $('#select-race').val(),
+            ethnicity: $('#select-ethnicity').val(),
+            education: $('#select-education').val(),
+            gender: $('#select-gender').val(),
+        }, function(data) {
+            $('#map').spin(false);
+            console.log(data.ret.features[0].properties.GEOID10)
+        });
+    }
+
+    $('#btn-submit').on('click', show_map);
+
 })()
