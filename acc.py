99f5d2115e530328d5adaa788cd180b2b604b8b5
Shi Yin <joysword@me.com>
2014-10-01 12:38:23 -0500

    got rid of unused imports in acc.py

--- acc.py
+++ acc.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, url_for, jsonify
-import json
+from flask import Flask, render_template
 
-import os
 
 app = Flask(__name__)
 # app.debug = True
 
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+
 
 @app.route('/about')
 def about():
