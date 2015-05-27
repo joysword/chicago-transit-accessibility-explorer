9fa30d473f9de72d6c38289aef9cc5e09a50c89e
Shi Yin <joysword@me.com>
2015-05-27 00:10:49 -0500

    added welcome pop up

---
+++ static/js/overlay.js
@@ -0,0 +1,36 @@
+
+// https://kopepasah.com/tutorial/awesome-overlays-with-simple-css-javascript-html/
+(function () {
+    var overlay = 'overlay-1'
+    var id = '#' + overlay
+    $(id).addClass('overlay-open');
+    $('body').addClass('overlay-view');
+
+    /**
+     * When the overlay outer wrapper or `overlay-close`
+     * trigger is clicked, let's remove the classes from
+     * the current overlay and body. Removal of these
+     * classes restores the current state of the user
+     * experience. Again, all handled by CSS (awesome).
+     */
+    $(id).on('click', function(e) {
+        // Verify that only the outer wrapper was clicked.
+        if (e.target.id == overlay) {
+            $(id).removeClass('overlay-open');
+            $('body').removeClass('overlay-view');
+        }
+    });
+
+    /**
+     * Closes the overlay when the esc key is pressed. See
+     * comment above on closing the overlay for more info
+     * on how this is accomplished.
+     */
+    $(document).keyup(function(e) {
+        // Verify that the esc key was pressed.
+        if (E.keyCode == 27) {
+            $(id).removeClass('overlay-open');
+            $('body').removeClass('overlay-view');
+        }
+    });
+})();
